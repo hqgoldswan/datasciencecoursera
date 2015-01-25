85e95938bf167057f125b5ad3fd18f541499895d
hqgoldswan <ongvang.it@gmail.com>
2015-01-26 01:29:08 +0900

    Update run_analysis.R
    Update the file path for the correct path comparing with the requirement from Coursera page.

--- Getting_and_Cleaning_Data/run_analysis.R
+++ Getting_and_Cleaning_Data/run_analysis.R
@@ -2,12 +2,12 @@
 library(plyr)
 
 # Step 1: Merge the training and test sets to create one data set
-x_train <- read.table("train/X_train.txt")
-y_train <- read.table("train/y_train.txt")
-subject_train <- read.table("train/subject_train.txt")
-x_test <- read.table("test/X_test.txt")
-y_test <- read.table("test/y_test.txt")
-subject_test <- read.table("test/subject_test.txt")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 # create 'x' data set
 x_data <- rbind(x_train, x_test)
 # create 'y' data set
@@ -16,7 +16,7 @@ y_data <- rbind(y_train, y_test)
 subject_data <- rbind(subject_train, subject_test)
 
 # Step 2: Extract only the measurements on the mean and standard deviation for each measurement
-features <- read.table("features.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
 # get only columns with mean() or std() in their names
 mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
 # subset the desired columns
@@ -25,7 +25,7 @@ x_data <- x_data[, mean_and_std_features]
 names(x_data) <- features[mean_and_std_features, 2]
 
 # Step 3: Use descriptive activity names to name the activities in the data set
-activities <- read.table("activity_labels.txt")
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")
 # update values with correct activity names
 y_data[, 1] <- activities[y_data[, 1], 2]
 # correct column name
